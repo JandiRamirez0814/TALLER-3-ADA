cc965b355594d881efd9f45dbd8b93877e677ab3
minizyke <maria.paula.lemus@correounivalle.edu.co>
2023-12-03 19:54:15 -0500

    uno

--- PUNTOS_TALLER/PUNTO_UNO/stooge_sort.py
+++ PUNTOS_TALLER/PUNTO_UNO/stooge_sort.py
@@ -1,35 +1,39 @@
+def stooge_sort(arr, i=0, j=None):
+    if j is None:
+        j = len(arr) - 1
+
+    if arr[i] > arr[j]:
+        arr[i], arr[j] = arr[j], arr[i]
+
+    if i + 1 >= j:
+        return
+
+    k = (j - i + 1) // 3
+
+    stooge_sort(arr, i, j - k)
+    stooge_sort(arr, i + k, j)
+    stooge_sort(arr, i, j - k)
+
+
+# Ejemplo de uso:
+arr = [5, 3, 7, 2, 8, 4, 1, 6]
+stooge_sort(arr)
+print("Lista ordenada:", arr)
+
 import random
 import time
 
-def stooge_sort(arr, l=0, h=None):
-    if h is None:
-        h = len(arr) - 1
 
-    if l >= h:
-        return
-
-    if arr[l] > arr[h]:
-        arr[l], arr[h] = arr[h], arr[l]
-
-    if h - l + 1 > 2:
-        t = (h - l + 1) // 3
-        stooge_sort(arr, l, h - t)
-        stooge_sort(arr, l + t, h - t)
-        stooge_sort(arr, l, h - t)
-
-def test_stooge_sort(array_size):
-    arr = [random.randint(1, 1000) for _ in range(array_size)]
-    start_time = time.time()
+def test_stooge_sort(tamano_arreglo):
+    arr = [random.randint(1, 1000) for _ in range(tamano_arreglo)]
+    tiempo_inicio = time.time()
     stooge_sort(arr)
-    end_time = time.time()
-    print(f"Tamaño del arreglo: {array_size}, Tiempo de ejecución: {end_time - start_time:.6f} segundos")
+    tiempo_fin = time.time()
+    print(f"Tamaño del arreglo: {tamano_arreglo}, Tiempo de ejecución: {tiempo_fin - tiempo_inicio:.6f} segundos")
 
-if __name__ == "_main_":
-    arr = [5, 3, 7, 2, 8, 4, 1, 6]
-    stooge_sort(arr)
-    print("Lista ordenada:", arr)
 
-    test_stooge_sort(10)
-    test_stooge_sort(100)
-    test_stooge_sort(1000)
-    test_stooge_sort(10000)
+# Realizar pruebas con diferentes tamaños de arreglo
+test_stooge_sort(10)
+test_stooge_sort(100)
+test_stooge_sort(1000)
+test_stooge_sort(10000)
